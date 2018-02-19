74d0c503ba9b35506b5c9ec9a65c094d464e2a10
Sasa Bolic <sasa.bolic@sixhours.io>
2018-02-19 10:46:00 +0100

    Initial commit

---
+++ src/main/java/com/vrs/videorentalstore/VideoRentalStoreApplication.java
@@ -0,0 +1,12 @@
+package com.vrs.videorentalstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoRentalStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoRentalStoreApplication.class, args);
+	}
+}
