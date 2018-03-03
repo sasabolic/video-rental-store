69a5656945f6683466319c99d223faf952befa87
Sasa Bolic <sasa.bolic@sixhours.io>
2018-03-03 14:50:51 +0100

    Move to new package

--- src/main/java/com/example/videorentalstore/film/ReleaseType.java
+++ src/main/java/com/example/videorentalstore/film/ReleaseType.java
@@ -1,16 +1,68 @@
-package com.example.videorentalstore.pricing;
+package com.example.videorentalstore.film;
 
 import java.math.BigDecimal;
 
-public enum ReleasePolicy {
+import static com.example.videorentalstore.film.Price.BASIC_PRICE;
+import static com.example.videorentalstore.film.Price.PREMIUM_PRICE;
+
+public enum ReleaseType {
 
     NEW_RELEASE {
-        
+        @Override
+        public BigDecimal calculatePrice(long daysRented) {
+            if (daysRented <= 0) {
+                return BigDecimal.ZERO;
+            }
+            return PREMIUM_PRICE.multiply(BigDecimal.valueOf(daysRented));
+        }
+
+        @Override
+        public int calculateBonusPoints() {
+            return 2;
+        }
     },
 
-    REGULAR_RELEASE,
+    REGULAR_RELEASE {
+        @Override
+        public BigDecimal calculatePrice(long daysRented) {
+            if (daysRented <= 0) {
+                return BigDecimal.ZERO;
+            }
 
-    OLD_RELEASE;
+            BigDecimal price = BASIC_PRICE;
+
+            if (daysRented > 3) {
+                price = price.add(BASIC_PRICE.multiply(BigDecimal.valueOf(daysRented - 3)));
+            }
+            return price;
+        }
+
+        @Override
+        public int calculateBonusPoints() {
+            return 1;
+        }
+    },
+
+    OLD_RELEASE {
+        @Override
+        public BigDecimal calculatePrice(long daysRented) {
+            if (daysRented <= 0) {
+                return BigDecimal.ZERO;
+            }
+
+            BigDecimal price = BASIC_PRICE;
+
+            if (daysRented > 5) {
+                price = price.add(BASIC_PRICE.multiply(BigDecimal.valueOf(daysRented - 5)));
+            }
+            return price;
+        }
+
+        @Override
+        public int calculateBonusPoints() {
+            return 1;
+        }
+    };
 
     public abstract BigDecimal calculatePrice(long daysRented);
 
