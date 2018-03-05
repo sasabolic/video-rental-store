0464a6cb794a4321e972dd404df61392814dedca
Sasa Bolic <sasa.bolic@sixhours.io>
2018-03-05 11:15:33 +0100

    Organize imports

--- src/test/java/com/example/videorentalstore/film/web/FilmControllerIntTest.java
+++ src/test/java/com/example/videorentalstore/film/web/FilmControllerIntTest.java
@@ -4,23 +4,14 @@ import com.example.videorentalstore.AbstractWebIntTest;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
-
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 public class FilmControllerIntTest extends AbstractWebIntTest {
 
