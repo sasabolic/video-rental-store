fe1d15872fe5b40830025b0b6a28fdf26a3db3c2
Sasa Bolic <sasa.bolic@sixhours.io>
2018-03-05 15:30:06 +0100

    Add test to check if controllers are initialized when application starts

---
+++ src/main/java/com/example/videorentalstore/rental/web/RentalController.java
@@ -0,0 +1,7 @@
+package com.example.videorentalstore.rental.web;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RentalController {
+}
--- src/test/java/com/example/videorentalstore/VideoRentalStoreApplicationTests.java
+++ src/test/java/com/example/videorentalstore/VideoRentalStoreApplicationTests.java
@@ -1,16 +1,39 @@
 package com.example.videorentalstore;
 
+import com.example.videorentalstore.customer.web.CustomerController;
+import com.example.videorentalstore.film.web.FilmController;
+import com.example.videorentalstore.rental.web.CustomerRentalController;
+import com.example.videorentalstore.rental.web.RentalController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class VideoRentalStoreApplicationTests {
 
+	@Autowired
+	private CustomerController customerController;
+
+	@Autowired
+	private FilmController filmController;
+
+	@Autowired
+	private RentalController rentalController;
+
+	@Autowired
+	private CustomerRentalController customerRentalController;
+
 	@Test
-	public void contextLoads() {
+	public void whenContextLoadThenControllersCreated() {
+		assertThat(customerController).isNotNull();
+		assertThat(filmController).isNotNull();
+		assertThat(rentalController).isNotNull();
+		assertThat(customerRentalController).isNotNull();
 	}
 
 }
