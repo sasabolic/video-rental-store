7058801c19b8946face3371b8f34f0027079c281
Sasa Bolic <sasa.bolic@sixhours.io>
2018-03-05 11:14:45 +0100

    Remove unused imports

--- src/test/java/com/example/videorentalstore/customer/CustomerRepositoryTest.java
+++ src/test/java/com/example/videorentalstore/customer/CustomerRepositoryTest.java
@@ -10,7 +10,6 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.math.BigDecimal;
-import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
