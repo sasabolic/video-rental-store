a36a7b5eebcd626ea3a5fcf4f9e1547873c9f585
Sasa Bolic <sasa.bolic@sixhours.io>
2018-03-08 21:44:52 +0100

    Update GenericResponseAssembler

--- src/main/java/com/example/videorentalstore/core/GenericResponseAssembler.java
+++ src/main/java/com/example/videorentalstore/core/GenericResponseAssembler.java
@@ -4,11 +4,29 @@ import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * The interface Generic response assembler.
+ *
+ * @param <E> the type parameter for entity object
+ * @param <D> the type parameter for DTO object
+ */
 public interface GenericResponseAssembler<E, D> {
 
-   D of(E entity);
+    /**
+     * Transforms entity object to DTO object.
+     *
+     * @param entity the entity
+     * @return the d
+     */
+    D of(E entity);
 
-   default List of(final Collection<E> entities) {
+    /**
+     * Transforms collection of entity objects to list of DTO objects.
+     *
+     * @param entities the entities
+     * @return the list
+     */
+    default List<D> of(final Collection<E> entities) {
        return entities.stream()
                .map(this::of)
                .collect(Collectors.toList());
