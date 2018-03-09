24341eb03c6a6143be5578978465ad5149486d32
Sasa Bolic <sasa.bolic@sixhours.io>
2018-03-09 13:31:52 +0100

    Update tests

--- src/test/java/com/example/videorentalstore/rental/web/dto/BatchRentalRequestValidationTest.java
+++ src/test/java/com/example/videorentalstore/rental/web/dto/BatchRentalRequestValidationTest.java
@@ -1,7 +1,5 @@
 package com.example.videorentalstore.rental.web.dto;
 
-import com.example.videorentalstore.rental.web.dto.ReturnBackRentalRequest;
-import com.example.videorentalstore.rental.web.dto.ReturnBackRentalRequestList;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -15,7 +13,7 @@ import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class ReturnBackRentalRequestListValidationTest {
+public class BatchRentalRequestValidationTest {
 
     private static Validator validator;
 
@@ -27,37 +25,47 @@ public class ReturnBackRentalRequestListValidationTest {
 
     @Test
     public void whenRequestValidThenViolationListEmpty() {
-        ReturnBackRentalRequestList request = new ReturnBackRentalRequestList(Arrays.asList(new ReturnBackRentalRequest(1L)));
-        final Set<ConstraintViolation<ReturnBackRentalRequestList>> validate = validator.validate(request);
+        BatchRentalRequest request = new BatchRentalRequest(BatchRentalRequest.Action.PAY.name(), Arrays.asList(new ReturnBackRentalRequest(1L)));
+        final Set<ConstraintViolation<BatchRentalRequest>> validate = validator.validate(request);
 
         assertThat(validate).isEmpty();
     }
 
     @Test
-    public void whenRequestNullThenViolationListNotEmpty() {
-        ReturnBackRentalRequestList request = new ReturnBackRentalRequestList(null);
-        final Set<ConstraintViolation<ReturnBackRentalRequestList>> validate = validator.validate(request);
+    public void whenRequestInvalidActionThenViolationForAction() {
+        BatchRentalRequest request = new BatchRentalRequest(null, Arrays.asList(new ReturnBackRentalRequest(1L)));
+        final Set<ConstraintViolation<BatchRentalRequest>> validate = validator.validate(request);
+
+        assertThat(validate).isNotEmpty();
+        assertThat(validate).hasSize(1);
+        assertThat(validate).extracting(ConstraintViolation::getMessage).containsOnly("Invalid action: 'null'. Allowed values are: 'PAY, RETURN, EXTRA_PAY'");
+    }
+
+    @Test
+    public void whenRequestEmptyRentalsThenViolationListNotEmpty() {
+        BatchRentalRequest request = new BatchRentalRequest(BatchRentalRequest.Action.PAY.name(), Collections.emptyList());
+        final Set<ConstraintViolation<BatchRentalRequest>> validate = validator.validate(request);
+
+        assertThat(validate).isNotEmpty();
+        assertThat(validate).hasSize(1);
+        assertThat(validate).extracting(ConstraintViolation::getMessage).containsOnly("List of rental requests cannot be empty");
+    }
+
+    @Test
+    public void whenRequestNullRentalsThenViolationForCreateRentalRequest() {
+        BatchRentalRequest request = new BatchRentalRequest(BatchRentalRequest.Action.PAY.name(), null);
+        final Set<ConstraintViolation<BatchRentalRequest>> validate = validator.validate(request);
+
+        assertThat(validate).isNotEmpty();
+        assertThat(validate).hasSize(1);
+    }
+
+    @Test
+    public void whenRequestInvalidAllFieldsThenViolationListNotEmpty() {
+        BatchRentalRequest request = new BatchRentalRequest(null, null);
+        final Set<ConstraintViolation<BatchRentalRequest>> validate = validator.validate(request);
 
         assertThat(validate).isNotEmpty();
         assertThat(validate).hasSize(2);
-        assertThat(validate).extracting(ConstraintViolation::getMessage).containsOnly("List of return rental requests cannot be null", "List of return rental requests cannot be empty");
-    }
-
-    @Test
-    public void whenRequestEmptyThenViolationListNotEmpty() {
-        ReturnBackRentalRequestList request = new ReturnBackRentalRequestList(Collections.emptyList());
-        final Set<ConstraintViolation<ReturnBackRentalRequestList>> validate = validator.validate(request);
-
-        assertThat(validate).isNotEmpty();
-        assertThat(validate).hasSize(1);
-        assertThat(validate).extracting(ConstraintViolation::getMessage).containsOnly("List of return rental requests cannot be empty");
-    }
-    @Test
-    public void whenRequestInvalidCreateRentalRequestThenViolationForCreateRentalRequest() {
-        ReturnBackRentalRequestList request = new ReturnBackRentalRequestList(Arrays.asList(new ReturnBackRentalRequest(null)));
-        final Set<ConstraintViolation<ReturnBackRentalRequestList>> validate = validator.validate(request);
-
-        assertThat(validate).isNotEmpty();
-        assertThat(validate).hasSize(1);
     }
 }
