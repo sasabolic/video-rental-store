27c9e50298114111b90e21bbcf1aa98bb8e3a4de
Sasa Bolic <sasa.bolic@sixhours.io>
2018-03-09 03:46:18 +0100

    Remove unused imports

--- src/test/java/com/example/videorentalstore/film/web/FilmControllerTest.java
+++ src/test/java/com/example/videorentalstore/film/web/FilmControllerTest.java
@@ -23,7 +23,6 @@ import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.isA;
 import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
