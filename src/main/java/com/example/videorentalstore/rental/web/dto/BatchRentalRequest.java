2ab26e3896d427a2b24a6804d92caebc5115a5e2
Sasa Bolic <sasa.bolic@sixhours.io>
2018-03-09 13:35:22 +0100

    Add new request DTO

---
+++ src/main/java/com/example/videorentalstore/rental/web/dto/BatchRentalRequest.java
@@ -0,0 +1,39 @@
+package com.example.videorentalstore.rental.web.dto;
+
+import com.example.videorentalstore.core.ValidAction;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
+import java.util.List;
+
+/**
+ * Batch rental request DTO.
+ */
+@Getter
+public class BatchRentalRequest {
+
+    public enum Action {
+        PAY,
+
+        RETURN,
+
+        EXTRA_PAY
+    }
+
+    @ValidAction
+    private String action;
+
+    @NotEmpty(message = "List of rental requests cannot be empty")
+    @Valid
+    private List<ReturnBackRentalRequest> rentals;
+
+    @JsonCreator
+    public BatchRentalRequest(@JsonProperty("action") String action,
+                              @JsonProperty("rentals") List<ReturnBackRentalRequest> rentals) {
+        this.action = action;
+        this.rentals = rentals;
+    }
+}
