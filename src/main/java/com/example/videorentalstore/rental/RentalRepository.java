1cb92d09686e00728b9b5ccb16c6fa357f9faf80
Sasa Bolic <sasa.bolic@sixhours.io>
2018-03-10 23:14:08 +0100

    Remove active field check for rental repository

--- src/main/java/com/example/videorentalstore/rental/RentalRepository.java
+++ src/main/java/com/example/videorentalstore/rental/RentalRepository.java
@@ -1,22 +1,9 @@
 package com.example.videorentalstore.rental;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 /**
  * Repository interface to manage {@code Rental} instances.
  */
-@Transactional(readOnly = true)
 public interface RentalRepository extends JpaRepository<Rental, Long> {
-
-    @Query("SELECT r FROM Rental r WHERE r.active = true")
-    @Override
-    List<Rental> findAll();
-
-    @Query("SELECT COUNT(r) FROM Rental r WHERE r.active = true")
-    @Override
-    long count();
 }
