a5468fcac41507d2d5de27cb8bc8e82cd3f4e918
Sasa Bolic <sasa.bolic@sixhours.io>
2018-03-12 04:53:37 +0100

    Delete assembler

--- src/main/java/com/example/videorentalstore/pricing/web/dto/assembler/ReceiptResponseAssembler.java
+++
@@ -1,8 +0,0 @@
-package com.example.videorentalstore.rental.web.dto.assembler;
-
-import com.example.videorentalstore.core.GenericResponseAssembler;
-import com.example.videorentalstore.rental.Receipt;
-import com.example.videorentalstore.rental.web.dto.ReceiptResponse;
-
-public interface ReceiptResponseAssembler extends GenericResponseAssembler<Receipt, ReceiptResponse> {
-}
