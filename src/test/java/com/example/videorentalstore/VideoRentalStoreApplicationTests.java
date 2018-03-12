814d85409f727a0d261c54d426fa71c8c383bf50
Sasa Bolic <sasa.bolic@sixhours.io>
2018-03-12 18:31:41 +0100

    Update test with new controllers

--- src/test/java/com/example/videorentalstore/VideoRentalStoreApplicationTests.java
+++ src/test/java/com/example/videorentalstore/VideoRentalStoreApplicationTests.java
@@ -2,6 +2,10 @@ package com.example.videorentalstore;
 
 import com.example.videorentalstore.customer.web.CustomerController;
 import com.example.videorentalstore.film.web.FilmController;
+import com.example.videorentalstore.invoice.web.CustomerInvoiceController;
+import com.example.videorentalstore.payment.web.CustomerPaymentController;
+import com.example.videorentalstore.payment.web.CustomerReceiptController;
+import com.example.videorentalstore.payment.web.ReceiptController;
 import com.example.videorentalstore.rental.web.CustomerRentalController;
 import com.example.videorentalstore.rental.web.RentalController;
 import org.junit.Test;
@@ -23,17 +27,33 @@ public class VideoRentalStoreApplicationTests {
 	private FilmController filmController;
 
 	@Autowired
-	private RentalController rentalController;
+	private CustomerInvoiceController customerInvoiceController;
+
+	@Autowired
+	private CustomerPaymentController customerPaymentController;
+
+	@Autowired
+	private CustomerReceiptController customerReceiptController;
+
+	@Autowired
+	private ReceiptController receiptController;
 
 	@Autowired
 	private CustomerRentalController customerRentalController;
+
+	@Autowired
+	private RentalController rentalController;
 
 	@Test
 	public void whenContextLoadThenControllersCreated() {
 		assertThat(customerController).isNotNull();
 		assertThat(filmController).isNotNull();
-		assertThat(rentalController).isNotNull();
+		assertThat(customerInvoiceController).isNotNull();
+		assertThat(customerPaymentController).isNotNull();
+		assertThat(customerReceiptController).isNotNull();
+		assertThat(receiptController).isNotNull();
 		assertThat(customerRentalController).isNotNull();
+		assertThat(rentalController).isNotNull();
 	}
 
 }
