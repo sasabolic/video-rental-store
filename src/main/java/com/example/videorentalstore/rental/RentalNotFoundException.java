8e0a297cf2a239435917aedf5894100d7dcdd045
Sasa Bolic <sasa.bolic@sixhours.io>
2018-05-30 17:54:23 +0200

    Update javadoc

--- src/main/java/com/example/videorentalstore/rental/RentalNotFoundException.java
+++ src/main/java/com/example/videorentalstore/rental/RentalNotFoundException.java
@@ -2,6 +2,8 @@ package com.example.videorentalstore.rental;
 
 /**
  * Exception thrown if {@link Rental} is not found.
+ *
+ * @author Sasa Bolic
  */
 public class RentalNotFoundException extends RuntimeException {
 
